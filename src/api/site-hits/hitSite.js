8c9baa827ef8e941220915a28210f73f1b44d936
Gustavo Diniz <dinizmonteirogustavo@gmail.com>
2022-07-12 09:37:53 -0300

    feat: Add error flow to hit function.
    Add error flow with body and response code for hit function.

--- src/api/site-hits/hitSite.js
+++ src/api/site-hits/hitSite.js
@@ -4,16 +4,39 @@ const countapi = require('countapi-js');
 require('dotenv').config();
 
 const hit = async (_) => {
+  const res = await handleHit()
+  if (res.requestSuceess) {
+    return resSuccess(res.hitCount)
+  }
+  return resError()
+};
+
+const handleHit = async() => {
   const result = await countapi.hit(process.env.CountApiKey)
   return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Access registred',
-        input: result,
-      }
-    ),
-  };
+    requestSuceess: result.status == 200,
+    hitCount: result.value
+  }
 };
+
+
+const resSuccess = hitCount => ({
+  statusCode: 200,
+  body: JSON.stringify(
+    {
+      message: 'Access registred',
+      hitCount
+    }
+  )
+})
+
+const resError = () => ({
+  statusCode: 400,
+  body: JSON.stringify(
+    {
+      message: 'Unable to register access'
+    }
+  )
+})
 
 module.exports = { hit }
